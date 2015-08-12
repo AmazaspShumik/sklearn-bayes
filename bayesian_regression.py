baf097abc82394e1c56326fb3c22aba8cb1845cd
amazaspshaumyan <sha-amazasp@yandex.ru>
2015-08-12 18:23:42 -0400

    added EM method

--- bayesian_regression.py
+++ bayesian_regression.py
@@ -4,7 +4,7 @@ import numpy as np
 from scipy.stats import multivariate_normal as mvn
 
 
-class BayesionRegression(object):
+class BayesianRegression(object):
     '''
     Bayesian Regression with type maximum likelihood for determining point estimates
     for precision variables alpha and beta, where alpha is precision of prior of weights
@@ -27,23 +27,32 @@ class BayesionRegression(object):
     def __init__(self,X,Y, thresh = 1e-5):
         
         # center input data for simplicity of further computations
-        self.X                 =  self._center(X)
-        self.Y                 =  self._center(Y)
+        self.X,self.mu_X       =  self._center(X)
+        self.Y,self.mu_Y       =  self._center(Y)
         self.thresh            =  thresh
         
         # to speed all further computations save svd decomposition and reuse it later
-        self.u,self.d,self.v   =  np.linalg.svd(self.X)
+        self.u,self.d,self.v   =  np.linalg.svd(self.X, full_matrices = False)
         
+        # precision parameters, they are calculated during evidence approximation
+        self.alpha             = 0 
+        self.beta              = 0
+        
+        # mean and precision of posterior distribution of weights
+        self.w_mu              = 0
+        self.w_beta            = 0
         
     @staticmethod
-    def _center(X):
+    def _center(x):
         '''
         Centers data by removing mean, this simplifies further computation
         '''
-        if len(X.shape) > 1:
-            return X - np.mean(X, axis = 0)
+        if len(x.shape) > 1:
+            mu     = np.mean(x, axis = 0)
+            return [x - mu,mu]
         else:
-            return X - np.mean(X)
+            mu     = np.mean(x)
+            return [x - np.mean(x),mu]
 
 
 
@@ -83,7 +92,7 @@ class BayesionRegression(object):
         return [w_mu,precision]
         
         
-    def _pred_dist_params(self,alpha,beta,x):
+    def _pred_dist_params(self,alpha,beta,x,w_mu,w_precision):
         '''
         Calculates parameters of predictive distribution
         
@@ -99,6 +108,13 @@ class BayesionRegression(object):
         x: numpy array of size 'unknown x m'
             Set of features for which corresponding responses should be predicted
             
+        w_mu: numpy array of size 'm x 1'
+            mean of posterior distribution of weights
+            
+        w_precision: numpy array of size 'm x 1'
+            precision of posterior distribution of weights
+        
+        
         Returns:
         ---------
         
@@ -106,11 +122,11 @@ class BayesionRegression(object):
             Parameters of univariate gaussian distribution [mean and variance] 
         
         '''
-        mu,precision =  self._coef_posterior(alpha,beta)
-        mu_pred      =  np.dot(x,mu)
-        S            =  np.dot(np.dot(self.v,np.diag(self.diag)),self.v.T) / beta
-        var          =  np.array([1.0/beta + np.dot(u,S.dot(u)) for u in x])
-        return [mu_pred,var]
+        mu_pred      =  np.dot(x,w_mu)
+        d            =  1/(self.d**2 + alpha/beta)
+        S            =  np.dot( np.dot(self.v.T, np.diag(d) ) , self.v)
+        var_pred     =  np.array([(1 + np.dot(u,S.dot(u)))/beta for u in x])
+        return [mu_pred,var_pred]
         
         
     def _em_evidence_approx(self, max_iter = 20):
@@ -146,16 +162,15 @@ class BayesionRegression(object):
             
     def _fixed_point_evidence_approx(self,max_iter):
         '''
-        Performs evidence approximation using fixed point algorithm, empirical
-        evidence show that this method is faster than EM
+        Performs evidence approximation using fixed point algorithm, finds precision 
+        parameters that maximize type II likelihood. Empirical evidence show that this 
+        method is faster than EM.
         
         Parameters:
         -----------
         
         max_iter: int
               Maximum number of iterations
-              
-        
         
         '''
         # number of observations and number of paramters in model
@@ -163,6 +178,9 @@ class BayesionRegression(object):
         
         # initial values of alpha and beta 
         alpha, beta = np.random.random(2)
+        
+        # store log likelihood at each iteration
+        log_likes   = []
         
         for i in range(max_iter):
             # find mean for posterior of w and gamma*
@@ -172,50 +190,64 @@ class BayesionRegression(object):
             p2_mu   =  np.dot(self.u.T,Y)
             mu      =  np.dot(p1_mu,p2_mu)
             
-            # store parameters to calculate change later
-            alpha_prev = alpha
-            beta_prev  = beta
             
             # use updated mu and gamma parameters to update alpha and beta
             alpha      =  gamma/np.dot(mu,mu)
             error      =  self.Y - np.dot(self.X,mu)
-            beta       =  (gamma - n)/np.dot(error,error)
+            beta       =  (n - gamma)/np.dot(error,error)
             
-            # change in parameters
-            delta_alpha = abs(alpha_prev - alpha)
-            delta_beta  = abs(beta_prev  - beta)
-            
-            # if change in parameters is less than threshold stop iterations
-            if delta_alpha < self.thresh and delta_beta < self.thresh:
-                break
+            # calculate log likelihood p(t | alpha, beta) (constants are not included)
+            normaliser =  m/2*np.log(alpha) + n/2*np.log(beta) - 1/2*np.sum(np.log(beta*d+alpha))
+            log_like   =  normaliser - alpha/2*np.dot(mu,mu) - beta/2*np.dot(error,error)            
+            log_likes.append(log_like)
+            # if change in log-likelihood is small terminate iterations
+            if i >=1:
+                delta_log_like = log_likes[-1] - log_likes[-2]
+                if delta_log_like < self.thresh:
+                    break
                 
-        return [alpha,beta]
+
+        print log_likes
+        self.alpha = alpha
+        self.beta  = beta
             
             
-    def fit(self, evidence_approx_method="fixed_point"):
+    def fit(self, evidence_approx_method="fixed_point",iterations = 100):
         '''
         Fits Bayesian linear regression
         '''
         # use type II maximum likelihood to find hyperparameters alpha and beta
         if evidence_approx_method == "fixed_point":
-            alpha, beta = self._fixed_point_evidence_approx(100)
+            self._fixed_point_evidence_approx(max_iter = iterations)
         elif evidence_approx_method == "EM":
-            alpha, beta = self._em_evidence_approx(100)
-            
+            self._em_evidence_approx(max_iter = iterations)
+
         # find parameters of posterior distribution
-        w_mu, w_beta = self._weights_posterior_params(alpha,beta)
+        self.w_mu, self.w_beta = self._weights_posterior_params(self.alpha,self.beta)
         
             
-    def predict_prob(self,Y,X):
-        pass
+    def predict(self,X, Y = None):
+        # find parameters of predictive distribution
+        mu,var = self._pred_dist_params(self.alpha,self.beta,X,self.w_mu,self.w_beta)
+        return mu,var
     
-    def predict_mean(self):
-        pass
+    def predict_mean(self,X):
+        '''
+        Returns mean of predictive distribution
+        
+        Para
+        '''
+        
     
 if __name__=="__main__":
     X      = np.ones([100,1])
+    Y      = np.ones([100,1])
     X[:,0] = np.linspace(1,10,100)
-    Y      = 4*X+5*np.random.random(100)
+    Y      = 4*X[:,0]+5*np.random.random(100)
+    br     = BayesianRegression(X,Y)
+    br.fit()
+    my,var = br.predict(X)
+    
     
     
     
