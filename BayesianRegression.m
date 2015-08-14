309d535daaed09e29eb504202a5b8bca7c894e2c
amazaspshaumyan <sha-amazasp@yandex.ru>
2015-08-14 01:02:27 -0400

    added posterior

--- BayesianRegression.m
+++ BayesianRegression.m
@@ -1,3 +1,7 @@
+% created by Amazasp Shuamyan
+
+
+
 classdef BayesianRegression
     % Implements Bayesian Regression with evidence approximation ( type II
     % maximum likelihood) using fixed-point algorithm and EM.
@@ -19,11 +23,11 @@ classdef BayesianRegression
         % method for evidence approximation
         evid_approx_method
         
-        % maximum number of iterations
-        max_iter
+        % maximum number of iterations and threshold for termination
+        max_iter, thresh
         
         % vector of log - likelihoods ( with exclusion of constants)
-        log
+        loglikes = ones(1);
         
     end
     
@@ -32,6 +36,14 @@ classdef BayesianRegression
         
         function obj = BayesianRegression(x,y,evid_approx,max_iter)
             % Contructor
+            % 
+            % Parameters:
+            %            x: matrix of size [n, m], matrix of explanatory variables
+            %            y: vector of size [n, 1], vector of dependent variables
+            %            evid_approx: str, method for evidence approximation,
+            %                         can be only 'EM' or 'fixed-point' 
+            %            max_iter: int, maximum number of iterations
+            
             obj.evid_approx_method   = evid_approx;
             obj.max_iter             = max_iter;
             [obj.n,obj.m]            = size(x);
@@ -42,25 +54,33 @@ classdef BayesianRegression
             [obj.U, obj.D, obj.V]    = svd(obj.X,'econ');
         end
         
+        
         function fit(obj)
-            % fits Bayesian Linear Regression
+            % Fits Bayesian Linear Regression with evidence approximation
             
-            % find point estimates for alpha and beta using evidence
-            % approximation method
+            % find point estimates for alpha and beta using evidence approximation
             [alpha,beta]        = obj.evidenceApproximation()
             
-            % Using alpha and beta, find parameters of posterior
-            % distribution of weights
+            % Finds parameters of posterior distribution of weights
             [w_mu, w_precision] = obj.posteriorDistParams()
         end
         
         function [prediction] = predict()
+            
+            
         end
     end
     
     methods(Access = 'private')
         
         function [alpha,beta] = evidenceApproximation(obj)
+        % Calculates alpha ( precision parameter of prior distribution) and
+        % beta ( precision parameter of likelihood) by maximising 
+        % p(y|X,alpha,beta).
+        %
+        % Returns:
+        %         alpha, beta - precison parameters ( both float)
+        %
             
             % initialise alpha and beta
             alpha = rand(); beta = rand();
@@ -102,20 +122,35 @@ classdef BayesianRegression
                 % not affect change in log-likelihood)
                 norm      = obj.m/2*log(alpha) + obj.n/2*log(beta) - 1/2*sum(log(beta*dsq+alpha));
                 loglike   = norm - alpha/2*(mu'*mu) - beta/2*sqdErr;
-                log_likes = [log_likes,loglike];
+                obj.loglikes = [obj.loglikes,loglike];
+                
+                % terminate iterations if change in log -likelihood is
+                % smaller than threshold
                 if i >= 2
-                    if log
-                        
-                return [alpha,beta]
-            end
-            
-            
-            
+                    if obj.loglikes(i) - obj.loglikes(i-1) < obj.thresh
+                        return 
+                    end
+                end
+            end 
         end
         
         
-        function posteriorDistParams(obj)
-            
+        function [mu,S] = posteriorDistParams(obj,alpha,beta)
+        % Calculates parameters of posterior distribution of weights.
+        % Since posterior distribution is Gaussian , it is characterised 
+        % by mean and precision.
+        % 
+        % Parameters:
+        %             alpha: float , precision of prior
+        %             beta: float, precision of likelihood
+        % 
+        % Returns:
+        %             mu: vector of size [obj.m x 1], posterior mean
+        %             S:  matrix of size [obj.m x obj.m], posterior
+        %                 precision
+        %   
+             mu = (obj.V'*
+             S  = beta*obj.X'*obj.X + alpha
             
         end
         
