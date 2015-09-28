5838d803eef1dcc30e643733468259ced10e8938
amazaspshaumyan <sha-amazasp@yandex.ru>
2015-09-28 19:07:50 -0400

    added predictions

--- bayesian_kernelised_log_reg.py
+++ bayesian_kernelised_log_reg.py
@@ -5,6 +5,9 @@ from scipy.optimize import fmin_l_bfgs_b
 
 
 def sigmoid(X):
+    '''
+    Evaluates sigmoid function
+    '''
     return 1. / ( 1 + np.exp(-X))
 
 
@@ -26,7 +29,7 @@ def cost_grad(X,Y,w,alpha):
     grad  = np.dot(X.T, s - Y) + alpha*w
     return [cost,grad]
     
-    
+
 #--------------------------- Bayesian Logistic Regression ------------------------#
     
 
@@ -64,23 +67,25 @@ class BayesianLogisticRegression(object):
        
     alpha_init: float (DEFAULT = None)
        Initial guess on precision parameter of prior, if not defined random guess
-       
+    
     '''
     
     
-    def __init__(self,X,Y,evidence_max_method = "fixed-point", max_iter_evidence_max = 100,
+    def __init__(self,X,Y,evidence_max_method = "fixed-point", max_iter_evidence     = 100,
                                                                max_iter_irls         = 20,
                                                                w_init                = None,
                                                                conv_thresh_evidence  = 1e-3,
                                                                conv_thresh_irls      = 1e-3,
                                                                alpha_init            = None):
-        self.X = X
+        self.mu_X = np.mean(X,0)
+        self.X    = X - self.mu_X
         # all classes in Y
-        classes = set(Y)
+        classes   = set(Y)
         # check that there are only two classes in vector Y
         assert len(classes)==2,"Number of classes in dependent variable should be 2"
         # convert dependent variable to 0 1 vector
-        
+        self.Y    = self._binarise(Y, classes)
+        # check that optimisation algorithm is set correctly
         assert evidence_max_method in ['fixed-point','EM'], 'Can be either "fixed-point" or "EM"'
         self.evidence_max_method    = evidence_max_method
         self.max_iter_evidence      = max_iter_evidence
@@ -91,8 +96,11 @@ class BayesianLogisticRegression(object):
            self.w_init              = np.random.random()
            self.alpha_init          = np.random.random()
            
-        # dimensionality of input 
+        # dimensionality & number of inputs 
         self.m                      = self.X.shape[1]
+        self.n                      = self.X.Shape[0]
+        # list of values for type II likelihood
+        self.evid                   = []
         
         
     def fit(self):
@@ -102,25 +110,88 @@ class BayesianLogisticRegression(object):
         self._evidence_maximize()
         
         
+    def predict(self,X):
+        '''
+        Predicts target value for new observations
+        
+        Parameters:
+        -----------
+        X: numpy array of size 'unknown x m'
+           Matrix of explanatory variables for test data
+           
+        Returns:
+        --------
+        : numpy array of size 'unknown x 1'
+           Vector of estimated target values
+        '''
+        probs          = self.predict_prob(X)
+        y              = np.zero(X.shape[0])
+        y[probs > 0.5] = 1 
+        return self._inverse_binarise(y)
+        
+        
     def predict_prob(self,X):
         '''
         Predicts probability of observation being of particular class
         
         Parameters:
         -----------
-        
         X: numpy array of size 'unknown x m'
-           Matrix of explanatory variables test data
+           Matrix of explanatory variables for test data
            
         Returns:
         --------
-        
         P: numpy array of size 'unknown x 1'
-           Matrix of probabilities
+           Vector of probabilities
         '''
+        x      = X - self.mu_X 
+        mu     = np.dot(x,self.Wmap)
+        u,d,vt = self.svdA
+        D     = np.diag( np.dot(vt.T,np.diag(d)), u.T)
+        sigma = np.dot(np.dot(x,D),x.T)
+        ks = 1. / ( 1. + np.pi*sigma**2 / 8)**0.5
+        return sigmoid(mu*ks)
         
-    
-
+        
+    def _evidence_maximize(self):
+        '''
+        Maximize evidence (type II maximum likelihood) 
+        '''
+        # initial guess on paramters
+        alpha_old    = self.alpha_init
+        Wmap_old     = self.w_init
+        evid_old     = np.NINF
+                
+        # evidence maximization iterative procedure
+        for i in range(self.max_iter_evidence):
+            
+            # find mean & covariance of Laplace approximation to posterior
+            Wmap, A,d         = self._irls(self.X, self.Y, alpha_old, Wmap_old)            
+            mu_sq             = np.dot(Wmap,Wmap) 
+            
+            # use EM or fixed-point procedures to update parameters            
+            if self.evidence_max_method == "EM":
+                alpha = self.m / (mu_sq + np.sum(d)) 
+            else:
+                gamma = np.sum((d - alpha) / d)
+                alpha = gamma / mu_sq
+                
+            # calculate value of type II maximum likelihood
+            evid = self._evidence_value(d,alpha,Wmap)
+            
+            # check termination conditions, if true write optimal values of 
+            # parameters to instance variables
+            if evid - evid_old < self.conv_thresh_evidence or i==self.max_iter_evidence - 1:
+                self.alpha = alpha
+                self.Wmap  = Wmap
+                self.A     = A
+                self.svdA  = np.linalg.svd(A, full_matrices = False)
+                return 
+            
+            evid_old  = evid
+            Wmap_old  = Wmap
+            
+            
     def _irls(self, X, Y, alpha, Theta):
         '''
         Iteratively refitted least squares method using l_bfgs_b.
@@ -147,48 +218,9 @@ class BayesianLogisticRegression(object):
         R          = s * (1 - s)
         negHessian = np.dot(X.T*R,X)
         negHessian.fill_diagonal(np.diag(negHessian + alpha))
-        u,d,vt     = np.linalg.svd(negHessian,full_matrices = False)
-        return [Wmap, negHessian, u, d, vt]
-        
-        
-    def _evidence_maximize(self):
-        '''
-        Maximize evidence (type II maximum likelihood) 
-        '''
-        # initial guess on paramters
-        alpha_old    = self.alpha_init
-        Wmap_old     = self.w_init
-        evid_old     = np.NINF
-                
-        # evidence maximization
-        for i in range(self.max_iter_evidence):
-            
-            # find mean & covariance of Laplace approximation to posterior
-            Wmap, A, u, d, vt = self._irls(self.X, self.Y, alpha_old, Wmap_old)            
-            mu_sq             = np.dot(Wmap,Wmap) 
-            
-            # use EM or fixed-point procedures to update parameters            
-            if self.evidence_max_method == "EM":
-                alpha = self.m / (mu_sq + np.sum(d)) 
-            else:
-                gamma = np.sum((d - alpha) / d)
-                alpha = gamma / mu_sq
-                
-            # calculate value of type II maximum likelihood
-            evid = self._evidence_value(d,alpha,Wmap)
-            
-            # check termination conditions, if true write optimal values of 
-            # parameters to instance variables
-            if evid - evid_old < self.conv_thresh_evidence or i==self.max_iter_evidence - 1:
-                self.alpha = alpha
-                self.Wmap  = Wmap
-                self.A     = A
-                return 
-            
-            evid_old  = evid
-            Wmap_old  = Wmap
-
-            
+        u,d,vt     = np.linalg.eig(negHessian)
+        return [Wmap, negHessian, d]
+ 
             
     def _evidence_value(self,d,alpha,Wmap):
         '''
@@ -205,8 +237,32 @@ class BayesianLogisticRegression(object):
         Z_normaliser    = 0.5*self.m*np.log(np.pi) - 0.5*np.log(np.sum(d))
         ED              = Likelihood_Wmap + Prior_Wmap + Z_normaliser
         return ED
-            
         
+        
+    def _binarise(self, Y, classes):
+        '''
+        Transform vector of two classes into binary vector
+        '''
+        self.inverse_encoding = {}
+        for el,val in zip(list(classes),[0,1]):
+            self.inverse_encoding[val] = el
+        y  =  np.zeros(Y.shape[0])
+        y[Y==self.inverse_encoding[1]] = 1
+        return y
+        
+    
+    def _inverse_binarise(self,y):
+        '''
+        Transform binary vector into vector of original classes
+        
+        Parameters:
+        -----------
+        y: numpy 
+        '''
+        Y = np.array([self.inverse_encoding[0] for e in range(y.shape[0])])
+        Y[y==1] = self.inverse_encoding[1]
+        return Y
+
         
 if __name__ == "__main__":
     X
