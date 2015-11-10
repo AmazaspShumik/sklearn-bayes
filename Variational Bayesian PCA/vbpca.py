de68352227f42bff1a3953b915647b8d21eb63ce
amazaspshaumyan <sha-amazasp@yandex.ru>
2015-11-10 15:53:29 +0300

    initial commit

---
+++ Variational Bayesian PCA/vbpca.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+
+class VBPCA(object):
+    '''
+    Variational Bayesian Principal Component Analysis.
+    
+    Automatically identifies 'real' dimensionality, uses Wishart prior for 
+    precision matrix
+    '''
+    
+    def __init__(self,X):
+        pass
+        
+    
+    def fit(self):
+        pass
