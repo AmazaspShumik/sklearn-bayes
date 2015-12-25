fixed precision overflow

Bayesian Linear Regression/bayesian_regression.py
@@ -16,14 +16,17 @@ class BayesianRegression(object):
        Matrix of explanatory variables (should not include bias term)
        
     Y: numpy arra of size 'n x 1'
-       Vector of dependent variables (we assume)
+       Vector of dependent variables.
        
     thresh: float
        Threshold for convergence for alpha (precision of prior)
        
+    lambda_0: float (DEAFAULT = 1e-6)
+       Prevents overflow of precision parameters (this is smallest value RSS can have)
+       
     '''
     
-    def __init__(self,X,Y, bias_term = True, thresh = 1e-5):
+    def __init__(self,X,Y, bias_term = True, thresh = 1e-3, lambda_0 = 1e-6):
         
         # center input data for simplicity of further computations
         self.mu_X              =  np.mean(X,axis = 0)
@@ -40,11 +43,15 @@ class BayesianRegression(object):
         # precision parameters, they are calculated during evidence approximation
         self.alpha             = None 
         self.beta              = None
+        self.lambda_0          = lambda_0
         
         # mean and precision of posterior distribution of weights
         self.w_mu              = None
         self.w_precison        = None   # when value is assigned it is m x m matrix
         self.D                 = None   # covariance
+        
+        # log-likelihood
+        self.logLike           = [np.NINF]
 
             
     def fit(self, evidence_approx_method="fixed-point",max_iter = 100):
@@ -54,16 +61,14 @@ class BayesianRegression(object):
         
         Parameters:
         -----------
-        
         max_iter: int
             Number of maximum iterations
             
-        evidence_approx_method: str
-            Method for approximating evidence
+        evidence_approx_method: str (DEFAULT = 'fixed-point')
+            Method for approximating evidence, either 'fixed-point' or 'EM'
         
         Returns:
         --------
-        
         parameters: dictionary 
                     - parameters['bias_term']  - coefficient for vector of constants
                     - parameters['weights']    - mean of posterior of weights
@@ -75,7 +80,7 @@ class BayesianRegression(object):
         # use type II maximum likelihood to find hyperparameters alpha and beta
         self._evidence_approx(max_iter = max_iter, method = evidence_approx_method)
 
-        # find parameters of posterior distribution after last ipdate of alpha & beta
+        # find parameters of posterior distribution after last update of alpha & beta
         self.w_mu, self.w_precision = self._posterior_params(self.alpha,self.beta)
         d                           =  1/(self.beta*self.d**2 + self.alpha)
         self.D                      =  np.dot( np.dot( self.v.T , np.diag(d) ) , self.v)
@@ -95,21 +100,19 @@ class BayesianRegression(object):
         
         Parameters:
         -----------
-        
         x: numpy array of size 'unknown x m'
             Set of features for which corresponding responses should be predicted
 
         
         Returns:
         ---------
-        
         :list of two numpy arrays (each has size 'unknown x 1')
             Parameters of univariate gaussian distribution [mean and variance] 
         
         '''
         x            =  x - self.mu_X
         mu_pred      =  np.dot(x,self.w_mu) + self.mu_Y
-        var_pred     =  1/self.beta + np.sum( np.dot( x, self.D )* x, axis = 1 )
+        var_pred     =  1/self.beta + np.sum( np.dot( x, self.D )* x, axis = 1)
         return [mu_pred,var_pred]
         
     
@@ -135,7 +138,7 @@ class BayesianRegression(object):
         return mu_pred  
         
                 
-    def _evidence_approx(self,max_iter = 100,method = "EM"):
+    def _evidence_approx(self,max_iter = 100,method = "fixed-point"):
         '''
         Performs evidence approximation , finds precision  parameters that maximize 
         type II likelihood. There are two different fitting algorithms, namely EM
@@ -152,15 +155,12 @@ class BayesianRegression(object):
               Can have only two values : "EM" or "fixed-point"
         
         '''
-        
         # number of observations and number of paramters in model
         n,m         = np.shape(self.X)
         
         # initial values of alpha and beta 
         alpha, beta = np.random.random(2)
-        
-        # store log likelihood at each iteration
-        log_likes   = []
+
         dsq         =  self.d**2
 
         
@@ -173,7 +173,7 @@ class BayesianRegression(object):
             
             # precompute errors, since both methods use it in estimation
             error   = self.Y - np.dot(self.X,mu)
-            sqdErr  = np.dot(error,error)
+            sqdErr  = max(np.dot(error,error),self.lambda_0)
             
             if method == "fixed-point":
      
@@ -181,18 +181,20 @@ class BayesianRegression(object):
                 gamma      =  np.sum(beta*dsq/(beta*dsq + alpha))
                
                 # use updated mu and gamma parameters to update alpha and beta
-                alpha      =  gamma/np.dot(mu,mu)
-                beta       =  (n - gamma)/sqdErr
+                alpha      =  ( gamma) / (np.dot(mu,mu) )
+                beta       =  (n - gamma )/sqdErr
                
             elif method == "EM":
                 
                 # M-step, update parameters alpha and beta
                 alpha      = m / (np.dot(mu,mu) + np.sum(1/(beta*dsq+alpha)))
                 beta       = n / ( sqdErr + np.sum(dsq/(beta*dsq + alpha)))
-            
+                
+                
             else:
                 raise ValueError("Only 'EM' and 'fixed-point' algorithms are implemented ")
             
+
             # after alpha & beta are updated last time we should also update mu
             p1_mu   =  np.dot(self.v.T, np.diag(self.d/(dsq+alpha/beta)))
             p2_mu   =  np.dot(self.u.T,self.Y)
@@ -201,16 +203,16 @@ class BayesianRegression(object):
             # calculate log likelihood p(Y | X, alpha, beta) (constants are not included)
             normaliser =  m/2*np.log(alpha) + n/2*np.log(beta) - 1/2*np.sum(np.log(beta*dsq+alpha))
             log_like   =  normaliser - alpha/2*np.dot(mu,mu) - beta/2*sqdErr           
-            log_likes.append(log_like)
+            self.logLike.append(log_like)
             
             # if change in log-likelihood is smaller than threshold stop iterations
             if i >=1:
-                if log_likes[-1] - log_likes[-2] < self.thresh:
+                if self.logLike[-1] - self.logLike[-2] < self.thresh:
                     break
         
         # write optimal alpha and beta to instance variables
         self.alpha = alpha
-        self.beta  = beta  
+        self.beta  = beta
         
               
     def _posterior_params(self,alpha,beta):
@@ -241,20 +243,10 @@ class BayesianRegression(object):
            Gaussian distribution
            
         '''
-        precision             = beta*np.dot(self.X.T,self.X)+alpha
+        precision             = beta*np.dot(self.X.T,self.X) + alpha
         self.diag             = self.d/(self.d**2 + alpha/beta)
         p1                    = np.dot(self.v.T,np.diag(self.diag))
         p2                    = np.dot(self.u.T,self.Y)
         w_mu                  = np.dot(p1,p2)
         return [w_mu,precision]
 
-        
-
-                      
-
-    
-    
-    
-    
-    
-    