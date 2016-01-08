5b599a773a24a1d10d0c81d578905429499c63af
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-01-08 17:48:01 +0300

    comments

--- Bayesian Logistic Regression/BayesianLogit.R
+++ Bayesian Logistic Regression/BayesianLogit.R
@@ -1,13 +1,4 @@
-# Bayesian logistic regression with local variational bounds
-#
-# Similar to standard Bayesian Logistic Regression, but uses local 
-# Jaakola-Jordan variational bound instead of Laplace approximation
-#
-# References:
-# 1) Bishop 2006, Pattern Recognition and Machine Learning ( Chapter 10 )
-# 2) Jaakola & Jordan 1994, 
-# 3) Murphy 2012,
-# 4) Barber 2015, Bayesian Reasoning and Machine Learning ()
+
 
 
 # ========================== Helper Functions ==================================
@@ -19,13 +10,41 @@ sigmoid <- function(x) {
 
 lambda <- function(x){
 	# helper function used for local variational bound calculation
-	-0.5 / x * ( sigmoid(x) - 0.5 )
+	return (-0.5 / x * ( sigmoid(x) - 0.5 ) )
 }
 
 % ======================= Bayesian Logistic Regression =========================
 
 
 BayesianLogisticRegression <- setClass(
+    # Bayesian logistic regression with local variational bounds.
+    # Similar to standard Bayesian Logistic Regression, but uses local 
+    # Jaakola-Jordan variational bound instead of Laplace approximation
+    #
+    # @Parameters:
+    # ============
+    # bias.term  : logical vector of size (1,1) [ DEFAULT = TRUE ]
+    #              If True adds columns of ones to matrix of explanatory variables
+    #
+    # max.iter   : numeric vector of size (1,1)  [ DEFAULT = 100 ]
+    #              Maximum number of iterations before convergence
+    #
+    # conv.thresh: numeric vector of size (1,1) [ DEFAULT = 1e-3 ]
+    #              Threshold for convergence of algorithm
+    #              
+    # w.mean0    : numeric vector of size ( number of features, 1)
+    #              Mean of prior distribution for coefficients
+    #
+    # w.prec0    : matrix of size (number of features, number of features)
+    #              Precision of prior distribution for coefficients
+    #
+    #           
+    # @References:
+    # ============
+    # 1) Bishop 2006, Pattern Recognition and Machine Learning ( Chapter 10 )
+    # 2) Jaakola & Jordan 1994, 
+    # 3) Murphy 2012,
+    # 4) Barber 2015, Bayesian Reasoning and Machine Learning ()
     
     # ----------------- set name of the class ------------------------
     
@@ -39,6 +58,8 @@ BayesianLogisticRegression <- setClass(
                   conv.thresh = 'numeric',
                   w.mean0     = 'numeric',
                   w.prec0     = 'numeric',
+                  coefs       = 'numeric',
+                  coefs.prec  = 'matrix',
                   N           = 'numeric',
                   M           = 'numeric'
                  )
@@ -48,10 +69,9 @@ BayesianLogisticRegression <- setClass(
     prototype = list(
                       bias.term   = TRUE,
                       max.iter    = 100,
-                      conv.thresh = 1e-5
-                      w.prec0     = 1e-6
-                      w.mean0     = 0
-                      N           = 0
+                      conv.thresh = 1e-5,
+                      w.prec0     = 1e-6,
+                      N           = 0,
                       M           = 0
                      )
                              
@@ -72,17 +92,34 @@ BayesianLogisticRegression <- setClass(
     #
     #      
     setMethod('fit',signature = c('BayesianLogisticRegression','matrix','numeric'), 
-              definition = function(theObject,X,y){
+              definition = function(theObject,X,Y){
               	
               	# check whether dimensionality is correct, if wrong change it
-              	if ( dim(X)[1] == theObject.N ) theObject.N = dim(X)[1]
-              	if ( dim(X)[2] == theObject.M ) theObject.M = dim(X)[2]
+              	if ( dim(X)[1] == theObject@N ) theObject@N = dim(X)[1]
+              	if ( dim(X)[2] == theObject@M ) theObject@M = dim(X)[2]
+              	N              =  theObject@N
+              	M              =  theObject@M
               	
               	# add bias term to matrix of explanatory variables if required
-              	if ( theObject@bias.term) X = 
+              	if ( theObject@bias.term) {
+              		newX       = matrix(data = NA, ncol = M + 1, nrow = N)
+              		newX[,1]   = rep(1,times = N)
+              		newX[,2:M] = X
+              		X          = newX
+              	}
               	
+              	# mean , precision and variational parameters
+              	w.mean0        = rep(0, times = M)
+              	w.prec0        = theObject@w.prec0
+              	eps            = runif(N)
               	
+              	# precompute some values before
+              	XY   = X %*% t( Y - 0.5 )
+              	
+              	# iterations for optimization
               	for( i in 1:theObject@max.iter){
+              		
+              		Sn = lambda(eps)
               		
               		
               		
