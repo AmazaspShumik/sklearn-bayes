b1c4525792de1f4317fd904a8a93604da09b30c8
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-02-09 02:34:58 +0300

    new examples & removed lower bound

--- Variational Bayesian Linear Regression/variational_lin_reg.py
+++ Variational Bayesian Linear Regression/variational_lin_reg.py
@@ -47,8 +47,7 @@ class VariationalLinearRegression(object):
     '''
     
     def __init__(self,X,Y, ab0 = [1e-6,1e-6], cd0 = [1e-6,1e-6], bias_term = True, max_iter = 50, 
-                                                                                   conv_thresh = 1e-5,
-                                                                                   verbose = True):
+                 conv_thresh = 1e-3, verbose = False):
         self.verbose          =  verbose
         self.bias_term        =  bias_term
         if bias_term is True:
@@ -93,17 +92,15 @@ class VariationalLinearRegression(object):
         # compute X'*Y  &  Y'*Y to reuse in each iteration
         XY                     = np.dot(self.X.T,self.Y)
         YY                     = np.dot(self.Y,self.Y)
-        # saved for lower bound calculation
-        a_init                 = self.a
-        b_init                 = self.b
-        c_init                 = self.c
-        d_init                 = self.d
         
         # some parameters of Gamma distribution have closed form solution
         self.a                 = self.a + 0.5*self.m
         self.c                 = self.c + 0.5*self.n
         b,d                    = self.b,self.d
         
+        # initial mean of posterior for coefficients
+        Mw = 0
+                
         for i in range(self.max_iter):
             
             #  ----------   UPDATE Q(w)   --------------
@@ -113,6 +110,7 @@ class VariationalLinearRegression(object):
             e_alpha       = self._gamma_mean(self.a,b)
             
             # update parameters of Q(w)
+            Mw_old       = np.copy(Mw)
             Mw,Sigma     = self._posterior_dist_beta(e_tau, e_alpha,XY)
             
             #  ----------    UPDATE Q(alpha_)   ------------
@@ -131,18 +129,13 @@ class VariationalLinearRegression(object):
             d             = self.d + 0.5*(YY + XSX + XMw) - MwXY
             
             
-            # --------- Lower Bound and Convergence Check ---------
+            # --------- Convergence Check ---------
             
-            # lower bound calculation
-            self._lower_bound(YY,XMw,MwXY,XSX,Sigma,E_w_sq,a_init,b_init,c_init,d_init,
-                                                                                e_tau,
-                                                                                e_alpha,
-                                                                                b,d)
             if self.verbose is True:
-                print "Iteration {0} is completed, lower bound is {1}".format(i,self.lower_bound[-1])
+                print "Iteration {0} is completed".format(i)
+                
             # check convergence 
-            #converged = self._check_convergence()
-            converged = False
+            converged = self._check_convergence(Mw,Mw_old)
             
             # save fitted parameters
             if converged or i==(self.max_iter-1):
@@ -228,28 +221,6 @@ class VariationalLinearRegression(object):
         '''
         Calculates parameters of approximation of posterior distribution 
         of weights
-        
-        Parameters:
-        -----------
-        
-        E_lambda: float
-           Expectation of precision parameter of likelihood with respect to 
-           its factored distribution Q(lambda_)
-        
-        E_alpha: float
-           Expectation of precision parameter for weight distribution with
-           respect to its factored distribution Q(alpha_)
-        
-        Returns:
-        --------
-        : list of two numpy arrays [Mn,Sn]
-        
-        Mn: numpy array of size [n_features, 1]
-           Mean of posterior ditribution of weights
-            
-        Sn: numpy array of size [n_features, n_features]
-           Covariance of posterior distribution of weights 
-            
         '''
         # inverse eigenvalues of precision matrix
         Dinv = 1. / (e_tau*self.D**2 + e_alpha)
@@ -262,123 +233,20 @@ class VariationalLinearRegression(object):
         return [Mn,Sn]
         
         
-    def _check_convergence(self):
+    def _check_convergence(self,Mw,Mw_old):
         '''
         Checks convergence of Mean Field Approximation
-        
-        Returns:
-        -------
-        : bool
-          True if approximation converged , False otherwise
         '''
-        assert len(self.lower_bound) >=2,'There should be at least 2 values of lower bound'
-        if self.lower_bound[-1] - self.lower_bound[-2] < self.conv_thresh:
+        if np.sum( abs(Mw - Mw_old) > self.conv_thresh ) == 0:
             return True
         return False
-        
-        
-    def _lower_bound(self,YY,XMw,MwXY,XSX,Sigma,E_w_sq,a_init,b_init,c_init,d_init,e_tau,e_alpha,b,d):
-        '''
-        Calculates lower bound and writes it to instance variable
-        
-        Parameters:
-        -----------
-        YY: float
-            Dot product Y.T*Y
-            
-        XMw: float
-             L2 norm of X*Mw, where Mw - mean of posterior of weights
-            
-        MwXY: float
-             Product of posterior mean of weights (Mw) and X.T*Y
-             
-        XSX: float
-             Trace of matrix X*Sigma*X.T, where Sigma - covariance of posterior of weights
-             
-        Sigma: numpy array of size [self.m,self.m]
-             Covariance matrix for Qw(w)
-             
-        E_w_sq: numpy array of size [self.m , 1]
-             Vector of weight squares
-            
-        a_init: float
-           Initial shape parameter for Gamma distributed weights
-           
-        b_init: float
-           Initial rate parameter
-           
-        c_init: float
-           Initial shape parameter for Gamma distributed precision of likelihood
-           
-        d_init: float
-           Initial rate parameter
-        
-        e_tau: float
-             Mean of precision for noise parameter
-             
-        e_alpha: float
-             Vector of means of precision parameters for weight distribution
-        
-        b: float
-           Learned rate parameter of Gamma distribution
-        
-        d: float
-           Learned rate parameter of Gamma distribution
-        '''
-        # < log(tau) > & < log(alpha) >
-        e_log_tau   = psi(self.c) - np.log(d)
-        e_log_alpha = psi(self.a) - np.log(b)
-        # < log P(Y|Xw, tau^-1) >
-        like        = 0.5*self.n*e_log_tau - 0.5*e_tau*(YY + XMw + XSX) - MwXY
-        # < log P(w| alpha) >
-        weights     = 0.5*self.m*e_log_alpha - 0.5*E_w_sq
-        # < log P(alpha) >
-        alpha_prior = (a_init - 1)*e_log_alpha - e_alpha*b_init
-        # < log P(tau) > 
-        tau_prior   = (c_init - 1)*e_log_tau - e_tau*c_init
-        # < log q(w) >
-        q_w         = -0.5*np.linalg.slogdet(Sigma)[1]
-        # < log q(alpha)>
-        q_alpha     = self.a*np.log(b) + (self.a - 1)*e_log_alpha - e_alpha*b - gammaln(self.a)
-        # < log q(tau)>
-        q_tau       = self.c*np.log(d) + (self.c - 1)*e_log_tau - e_tau*d - gammaln(self.c)
-        # lower bound calculation
-        L           = like + weights + tau_prior + alpha_prior - q_w - q_alpha - q_tau
-        self.lower_bound.append(L)
         
         
     @staticmethod
     def _gamma_mean(a,b):
        '''
        Calculates mean of gamma distribution
-    
-       Parameters:
-       -----------
-       a0: float
-           Shape parameter
-    
-       b0: float
-           Rate parameters
-        
-       Returns:
-       --------
-       : float
-           Mean of gamma distribution
        '''
        return a/b
-       
-       
-       
-if __name__=='__main__':
-    X = np.array([[ 0.1,  -0.1,  -0.2,   0.02],
-                  [ 0.3,  -0.3,  -0.6,   0.06],
-               [ 0.4,  -0.4,  -0.8,   0.08],
-               [ 0.5,  -0.5,  -1.,    0.1 ]])
 
-    Y = np.array([ 0.2,  0.6,  0.8,  1. ])  
-    vr = VariationalLinearRegression(X,Y)
-    vr.fit()
-    y_hat = vr.predict(X)
-    print y_hat
- 
     
