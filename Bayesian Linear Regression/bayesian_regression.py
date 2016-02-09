dbbcd326d3af126e406e69fccb75a15d3f26e972
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-02-09 20:44:18 +0300

    scikit-learn api

--- Bayesian Linear Regression/bayesian_regression.py
+++ Bayesian Linear Regression/bayesian_regression.py
@@ -2,26 +2,35 @@
 
 import numpy as np
 from scipy.linalg import pinvh
+from scipy.linalg import svd
 import warnings
 
+from sklearn.base import RegressorMixin
+from sklearn.linear_model.base import LinearModel
+from sklearn.utils import check_X_y
 
-class BayesianRegression(object):
+
+
+class BayesianRegression(RegressorMixin,LinearModel):
     '''
     Bayesian Regression with type II maximum likelihood for determining point estimates
     for precision variables alpha and beta, where alpha is precision of prior of weights
     and beta is precision of likelihood
     
     Parameters:
-    -----------
-    
-    X: numpy array of size 'n x m'
-       Matrix of explanatory variables (should not include bias term)
-       
-    Y: numpy arra of size 'n x 1'
-       Vector of dependent variables.
-       
-    thresh: float
+    -----------  
+    n_iter: int, optional (DEFAULT = 300)
+       Maximum number of iterations
+         
+    tol: float, optional (DEFAULT = 1e-3)
        Threshold for convergence
+       
+    optimizer: str, optional (DEFAULT = 'fp')
+       Method for optimization , either Expectation Maximization or 
+       Fixed Point Gull-MacKay {'em','fp'}
+       
+    fit_intercept: bool, optional (DEFAULT = True)
+       If True includes bias term in model
        
     lambda_0: float (DEAFAULT = 1e-10)
        Prevents overflow of precision parameters (this is smallest value RSS can have).
@@ -30,221 +39,162 @@ class BayesianRegression(object):
        
     alpha: float (DEFAULT = 1e-6)
        Initial value of precision paramter for coefficients ( by default we define 
-       broad very broad distribution )
+       very broad distribution )
        
-    beta: float (DEFAULT = 1e-6)
-       Initial value of precision parameter for noise ( by default we define very 
-       broad distribution )
+       
+    Attributes
+    ----------
+    coef_  : array, shape = (n_features)
+        Coefficients of the regression model (mean of posterior distribution)
+        
+    alpha_ : float
+        Estimated precision of coefficients
+       
+    beta_  : float 
+        Estimated precision of noise
+
+    sigma_ : array, shape = (n_features, n_features)
+        Estimated covariance matrix of the coefficients
+
+    scores_: list
+        Values of log-likelihood
+
     '''
     
-    def __init__(self,X,Y, bias_term = True, thresh = 1e-3, lambda_0 = 1e-8, alpha = 1,
-                                                                             beta  = 1):
-        # center input data for simplicity of further computations
-        self.mu_X              =  np.mean(X,axis = 0)
-        self.X                 =  X - np.outer(self.mu_X, np.ones(X.shape[0])).T
-        self.mu_Y              =  np.mean(Y)
-        self.Y                 =  Y - np.mean(Y)
-        self.thresh            =  thresh
-        if bias_term is True:
-            self.bias_term     =  self.mu_Y
-        
-        # to speed all further computations save svd decomposition and reuse it later
-        self.u,self.d,self.v   =  np.linalg.svd(self.X, full_matrices = False)
-        
-        # precision parameters, they are calculated during evidence approximation
-        self.alpha             =  alpha 
-        self.beta              =  beta
-        self.lambda_0          =  lambda_0
-        
-        # mean and precision of posterior distribution of weights
-        self.w_mu              =  None
-        self.w_precison        =  None   # when value is assigned it is m x m matrix
-        self.D                 =  None   # covariance
-        
-        # log-likelihood
-        self.logLike           = [np.NINF]
-        self.perfect_fit       = False
+    def __init__(self,n_iter = 300, tol = 1e-3, optimizer = 'fp', 
+                 fit_intercept = True, lambda_0 = 1e-5, alpha = 1,
+                 copy_X = True, verbose = False):
+        self.n_iter        =  n_iter
+        self.tol           =  tol
+        if optimizer not in ['em','fp']:
+            raise ValueError('Optimizer can be either "em" of "fp" ')
+        self.optimizer     =  optimizer 
+        self.fit_intercept =  fit_intercept
+        self.alpha         =  alpha 
+        #self.beta          =  beta
+        self.lambda_0      =  lambda_0        
+        self.perfect_fit   =  False
+        self.copy_X        =  copy_X
+        self.verbose       =  verbose
 
             
-    def fit(self, evidence_approx_method="fixed-point",max_iter = 100):
+    def fit(self, X, y, evidence_approx_method="fp",max_iter = 100):
         '''
         Fits Bayesian linear regression, returns posterior mean and preision 
         of parameters
         
-        Parameters:
-        -----------
-        max_iter: int
-            Number of maximum iterations
-            
-        evidence_approx_method: str (DEFAULT = 'fixed-point')
-            Method for approximating evidence, either 'fixed-point' or 'EM'
-            
-        # Theory Note:
-        -----------------
-        This code implements two methods to fit type II ML Bayesian Linear Regression:
-        Expectation Maximization and Fixed Point Iterations. Expectation Maximization
-        is generally slower so by default we use fixed-point.     
+        Parameters
+        ----------
+        X: array-like of size [n_samples,n_features]
+           Matrix of explanatory variables (should not include bias term)
+       
+        Y: array-like of size [n_features]
+           Vector of dependent variables.
+           
+        Returns
+        -------
+        object: self
+          self
+    
         '''
-        # use type II maximum likelihood to find hyperparameters alpha and beta
-        self._evidence_approx(max_iter = max_iter, method = evidence_approx_method)
+        # preprocess data
+        X, y = check_X_y(X, y, dtype=np.float64, y_numeric=True)
+        n_samples, n_features = X.shape
+        X, y, X_mean, y_mean, X_std = self._center_data(X, y, self.fit_intercept,
+                                                        self.copy_X)
+        self._x_mean_  = X_mean
+        self._y_mean_  = y_mean
+        self._x_std_   = X_std
+        self.scores_   = [np.NINF]
+        
+        #  precision of noise & and coefficients
+        alpha   =  self.alpha
+        var_y  = np.var(y)
+        # check that variance is non zero !!!
+        if var_y == 0 :
+            beta = 1e-2
+        else:
+            beta = 1. / np.var(y)
 
-        # find parameters of posterior distribution after last update of alpha & beta
-        self.w_mu, self.w_precision = self._posterior_params(self.alpha,self.beta)
-        self.D                      = pinvh(self.w_precision)
+        # to speed all further computations save svd decomposition and reuse it later
+        u,d,v   = svd(X, full_matrices = False)
+        Uy      = np.dot(u.T,y)
+        dsq     =  d**2
+
+    
+        for i in range(self.n_iter):
+            
+            # find mean for posterior of w ( for EM this is E-step)
+            p1_mu   =  v.T * ( d/(dsq+alpha/beta) )
+            mu      =  np.dot(p1_mu,Uy)
+        
+            # precompute errors, since both methods use it in estimation
+            error   = y - np.dot(X,mu)
+            sqdErr  = np.dot(error,error)
+            
+            if sqdErr / n_samples < self.lambda_0:
+                self.perfect_fit = True
+                warnings.warn( ('Almost perfect fit!!! Estimated values of variance '
+                                'for predictive distribution are computed using only '
+                                'Residual Sum of Squares, terefore they do not increase '
+                                'in case of extrapolation')
+                             )
+                break
+            
+            if self.optimizer == "fp":           
+                gamma      =  np.sum(dsq/(dsq + alpha/beta))
+                # use updated mu and gamma parameters to update alpha and beta
+                alpha      =   gamma  / np.dot(mu,mu) 
+                beta       =  ( n_samples - gamma ) / sqdErr
+            else:             
+                # M-step, update parameters alpha and beta to maximize ML TYPE II
+                alpha      = n_features / (np.dot(mu,mu) + np.sum(1/(beta*dsq+alpha)))
+                beta       = n_samples / ( sqdErr + np.sum(dsq/(beta*dsq + alpha))  )
+                
+            # calculate log likelihood p(Y | X, alpha, beta) (constants are not included)
+            normaliser  =  0.5 * ( n_features*np.log(alpha) + n_samples*np.log(beta) )
+            normaliser -=  0.5 * np.sum(np.log(beta*dsq+alpha))
+            log_like    =  normaliser - 0.5*alpha*np.sum(mu**2) 
+            log_like   -=  0.5*beta*sqdErr - 0.5*n_samples*np.log(2*np.pi)         
+            self.scores_.append(log_like)
+            
+            if self.verbose:
+                print( ("Iteration {0} completed, value of log " 
+                        "likelihood is {1}".format(i,log_like) )
+                     ) 
+
+            # if change in log-likelihood is smaller than threshold terminate
+            converged = ( self.scores_[-1] - self.scores_[-2] < self.tol)
+            if converged or i==self.n_iter -1:
+                break
+
+        # pinvh is used for numerical stability (inverse has clased form solution)
+        self.sigma_ = pinvh( np.dot(v.T * (beta*dsq + alpha), v) )
+        self.coef_  = beta*np.dot(self.sigma_, np.dot(X.T,y))
+        self._set_intercept(X_mean,y_mean,X_std)
+        self.beta_  = beta
+        self.alpha_ = alpha
+        return self
             
 
-    def predict_dist(self,x):
+    def predict_dist(self,X):
         '''
         Calculates  mean and variance of predictive distribution at each data 
         point of test set.
         
-        Parameters:
-        -----------
-        x: numpy array of size 'unknown x m'
+        Parameters
+        ----------
+        x: array-like of size [n_test_samples, n_features]
             Set of features for which corresponding responses should be predicted
 
         
-        Returns:
-        ---------
-        :list of two numpy arrays (each has size 'unknown x 1')
+        Returns
+        -------
+        :list of two numpy arrays (each has size [n_test_samples])
             Parameters of univariate gaussian distribution [mean and variance] 
         
         '''
-        x            =  x - self.mu_X
-        Y_hat        =  np.dot(x,self.w_mu)
-        y_hat        =  Y_hat + self.mu_Y
-        if self.perfect_fit is False:
-            var_pred     =  1./self.beta + np.sum( np.dot( x, self.D )* x, axis = 1)
-        else:
-            var_pred     =  np.ones(x.shape[0])*np.sum( (self.Y - Y_hat)**2 ) / self.X.shape[0]
+        y_hat  = self._decision_function(X)
+        x      = (X - self._x_mean_) / self._x_std_
+        var_pred  =  1./self.beta_ + np.sum( np.dot( x, self.sigma_ )* x, axis = 1)
         return [y_hat,var_pred]
-        
-    
-    def predict(self,x):
-        '''
-        Calculates mean of predictive distribution at each data point of test set
-        
-        Parameters:
-        ----------
-        x: numpy array of size 'unknown x m'
-            Set of features for which corresponding responses should be predicted
-            
-        Returns:
-        --------
-        mu_pred: numpy array of size 'unknown x 1'
-                  Mean of predictive distribution
-        '''
-        x           = x - self.mu_X
-        mu_pred     = np.dot(x,self.w_mu) + self.mu_Y
-        return mu_pred  
-        
-                
-    def _evidence_approx(self,max_iter = 100,method = "fixed-point"):
-        '''
-        Performs evidence approximation , finds precision  parameters that maximize 
-        type II likelihood. There are two different fitting algorithms, namely EM
-        and fixed-point algorithm, empirical evidence shows that fixed-point algorithm
-        is faster than EM
-        
-        Parameters:
-        -----------
-        max_iter: int (DEFAULT = 100)
-              Maximum number of iterations
-        
-        method: str (DEFAULT = 'fixed-point')
-              Method that is used to fit model, can have only two values : "EM" or "fixed-point"
-        
-        '''
-         # number of observations and number of paramters in model
-        n,m         = np.shape(self.X)
-        
-        # initial values of alpha and beta 
-        alpha, beta = self.alpha, self.beta
-        dsq         = self.d**2
-
-    
-        for i in range(max_iter):
-            
-            # find mean for posterior of w ( for EM this is E-step)
-            p1_mu   =  np.dot(self.v.T, np.diag(self.d/(dsq+alpha/beta)))
-            p2_mu   =  np.dot(self.u.T, self.Y)
-            mu      =  np.dot(p1_mu,p2_mu)
-        
-            # precompute errors, since both methods use it in estimation
-            error   = self.Y - np.dot(self.X,mu)
-            sqdErr  = np.dot(error,error)
-            
-            if sqdErr / n < self.lambda_0:
-                self.perfect_fit = True
-                warnings.warn( 
-                ('Almost perfect fit!!! Estimated values of variance '
-                 'for predictive distribution are computed using only '
-                 'Residual Sum of Squares, terefore they do not increase '
-                 'in case of extrapolation')
-                             )
-                break
-            
-            if method == "fixed-point":           
-                # update gamma
-                gamma      =  np.sum(dsq/(dsq + alpha/beta))
-                # use updated mu and gamma parameters to update alpha and beta
-                alpha      =   gamma  / np.dot(mu,mu) 
-                beta       =  ( n - gamma ) / sqdErr
-               
-            elif method == "EM":             
-                # M-step, update parameters alpha and beta
-                alpha      = m / (np.dot(mu,mu) + np.sum(1/(beta*dsq+alpha)))
-                beta       = n / ( sqdErr + np.sum(dsq/(beta*dsq + alpha))  )
-            else:
-                raise ValueError("Only 'EM' and 'fixed-point' algorithms are implemented ")
-            
-            # calculate log likelihood p(Y | X, alpha, beta) (constants are not included)
-            normaliser =  0.5 * ( m*np.log(alpha) + n*np.log(beta) - np.sum(np.log(beta*dsq+alpha)))
-            log_like   =  normaliser - 0.5*alpha*np.sum(mu**2) - 0.5*beta*sqdErr - 0.5*n*np.log(2*np.pi)         
-            self.logLike.append(log_like)
-
-            # if change in log-likelihood is smaller than threshold stop iterations
-            # if squared error is below threshold termonate, to avoide overflow in beta
-            if i >=1:
-                if self.logLike[-1] - self.logLike[-2] == self.thresh:
-                    break
-        
-        # write optimal alpha and beta to instance variables
-        self.alpha = alpha
-        self.beta  = beta
-        
-        
-    def _posterior_params(self,alpha,beta):
-        '''
-        Calculates parameters of posterior distribution of weights and log-likelihhod
-        Uses economy svd for fast calculaltions.
-        
-        # Theory note:
-        ---------------------
-        Multiplying likelihood of data on prior of weights we obtain distribution 
-        proportional to posterior of weights. By completing square in exponent it 
-        is easy to prove that posterior distribution is Gaussian.
-        
-        Parameters:
-        -----------
-        
-        alpha: float
-            Precision parameter for prior distribution of weights
-            
-        beta: float
-            Precision parameter for noise distribution
-            
-        Returns:
-        --------
-        
-        : list of two numpy arrays
-           First element of list is mean and second is precision of multivariate 
-           Gaussian distribution
-           
-        '''
-        precision             = beta*np.dot(self.X.T,self.X) + alpha*np.eye(self.X.shape[1])
-        self.diag             = self.d/(self.d**2 + alpha/beta)
-        p1                    = np.dot(self.v.T,np.diag(self.diag))
-        p2                    = np.dot(self.u.T,self.Y)
-        w_mu                  = np.dot(p1,p2)
-        return [w_mu,precision]
