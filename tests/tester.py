cec87f7b12038fa6cd077f30e549891d7408b6af
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-02-14 18:23:47 +0300

    initial commit

---
+++ tests/tester.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from unittest import TestCase
+from sklearn.utils.estimator_checks import check_estimator
+from sklearn_bayes.bayes_linear.bayesian_regression import BayesianRegression
+
+
+
