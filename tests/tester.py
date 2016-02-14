e725d51945a5fefa8fe46ce064cf39a813b15af5
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-02-15 00:04:21 +0300

    checking build

--- tests/tester.py
+++ tests/tester.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
 from unittest import TestCase
-from sklearn.utils.estimator_checks import check_estimator
-from sklearn_bayes.bayes_linear.bayesian_regression import BayesianRegression
-
 
 
