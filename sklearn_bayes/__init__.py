f3555e31512062cbc749da75ac4f454e854c8b8d
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-02-16 02:48:58 +0300

    tutorials

--- sklearn_bayes/__init__.py
+++ sklearn_bayes/__init__.py
@@ -1,3 +1,37 @@
-'''sklearn_bayes: bayesian machine learning models with sklearn api'''
+'''
+Bayesian machine learning models with sklearn api
+=================================================
+
+   IMPLEMENTED ALGORITHMS:
+   -----------------------
+   
+       ** Regression 
+          - Relevance Vector Regression (version 2.0)
+          - Variational Relevance Vector Regression
+          - Type II ML Bayesian Linear Regression
+          - Variational Linear Regression
+          - Type II ML ARD Regression
+          - Variational ARD Regression
+          
+       ** Classification   
+          - Relevance Vector Classifier
+          - Type II ML Bayesian Logistic Regression (Laplace Approximation)
+          - Variational Logistic Regression (Local Variational Approximation)
+          - Type II ML ARD Classification
+          
+          
+    PACKAGE CONTENTS:
+    -----------------
+    
+        linear (package)
+        logistic (package)
+        rvm (package)
+        vrvm (package)
+        mixture (package)
+
+'''
+
+__all__ = ['rvm','linear','logistic','vrvm']
 
 __version__ = '0.1.0a1'
+
