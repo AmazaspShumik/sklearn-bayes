67e4bf1204e18413eb2ccad2339e735d0ccdf3ed
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-04-21 17:45:49 +0300

    add to package

---
+++ sklearn_bayes/hmm/__init__.py
@@ -0,0 +1,12 @@
+'''
+
+   Bayesian Hidden Markov Model (Beal 2003)
+   ========================================
+   Bernoulli emission probs: VBBernoulliHMM
+   Gaussian emission probs: VBGaussianHMM
+   Multinoulli emission probs: VBMultinoulliHMM 
+'''
+
+from .hmm import VBMultinoulliHMM ,VBGaussianHMM,VBBernoulliHMM
+
+__all__ = ['VBBernoulliHMM','VBGaussianHMM','VBMultinoulliHMM ']
