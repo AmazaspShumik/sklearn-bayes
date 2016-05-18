643acf17163c46adfbf29f7924d4fda896fe1cf9
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-05-18 22:57:12 +0300

    rvm\ard packaging

--- sklearn_bayes/rvm_ard_models/__init__.py
+++ sklearn_bayes/rvm_ard_models/__init__.py
@@ -8,9 +8,14 @@ Fast Relevance Vector Machine & ARD ( Tipping and Faul (2003) )
     Relevance Vector Classifier : RVC
     Classification ARD          : ClassificationARD
     Regression ARD              : RegressionARD
+    Variational Regression ARD  : VariationalRegressionARD
+    Variational Relevance Vector
+                     Regression : VRVR
 '''
 from .fast_rvm import RVR,RVC,ClassificationARD,RegressionARD
+from .vrvm import VariationalRegressionARD, VRVR
 
-__all__ = ['RVR','RVC','ClassificationARD','RegressionARD']
+__all__ = ['RVR','RVC','ClassificationARD','RegressionARD','VariationalRegressionARD',
+           'VRVR']
 
 
