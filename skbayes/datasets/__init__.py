9a877bb7dc1992f3f8ec49a73e6f24f9e9516393
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-06-04 14:23:25 +0300

    load data

--- skbayes/datasets/__init__.py
+++ skbayes/datasets/__init__.py
@@ -1,1 +1,18 @@
 __all__ = ['load_olf_faithful']
+
+import pandas as pd
+import numpy as np
+import os
+
+
+def load_old_faithful():
+    '''
+    Load Old Faithful Geyser data
+    '''
+    curdir = os.getcwd()
+    filepath = '/'.join([curdir,'old_faithful.txt']) 
+    data = pd.read_csv(filepath)
+    return {'X': np.array(data[['eruptions','waiting']]),
+            'columns': data.columns}
+
+
