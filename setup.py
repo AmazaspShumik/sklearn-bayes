77d6e16922c015633abd9c62aa740a06dc16bc6f
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-06-17 17:00:10 +0300

    another try repackaging

--- setup.py
+++ setup.py
@@ -5,16 +5,26 @@ from setuptools.extension import Extension
 from Cython.Build import cythonize
 import numpy as np
 
+try:
+    from numpy.distutils.misc_util import get_info
+except ImportError:
+    def get_info(name):
+        return {}
+
 ext_modules = [ Extension("skbayes.decomposition_models.gibbs_lda_cython",
                           ["skbayes/decomposition_models/gibbs_lda_cython.c"], 
-                          include_dirs = [np.get_include()],
-                          extra_compile_args=["-O3"])]
-           
-setup(
+                          #include_dirs = [np.get_include()],
+                          extra_compile_args=["-O3"],
+                          **get_info("npymath"))]
+import skbayes
+
+ 
+if __name__=='__main__':           
+   setup(
        name = 'skbayes',
        version  = '0.1.0a1',
        description = "bayesian machine learning algorithms with scikit-learn api",
-       url         = 'https://github.com/AmazaspShumik/sklearn-bayes',
+       url         = 'https://github.com/AmazaspShumik/sklearn-bayes/skbayes',
        author      = 'Amazasp Shaumyan',
        author_email = 'amazasp.shaumyan@gmail.com',
        license      = 'MIT',
@@ -33,5 +43,5 @@ setup(
           'Operating System :: Mac OS X',
           'Programming Language :: Python :: 2.7'],
        ext_modules = ext_modules
-     )
+   )
 
