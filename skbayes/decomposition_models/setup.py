8884d6a0231daa6a0b328b7bd51d05bce07e53b4
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-06-17 14:18:01 +0300

    another try repackaging

--- skbayes/decomposition_models/setup.py
+++ skbayes/decomposition_models/setup.py
@@ -1,12 +1,23 @@
-from distutils.core import setup
-from distutils.extension import Extension
-from Cython.Distutils import build_ext
 import numpy as np
+import os
+from numpy.distutils.misc_util import Configuration
 
-ext_modules=[ Extension("gibbs_lda_cython",["gibbs_lda_cython.pyx"])]
+
+def configuration(parent_package="", top_path=None):
+    config = Configuration("tree", parent_package, top_path)
+    libraries = []
+    if os.name == 'posix':
+        libraries.append('m')
+    config.add_extension("gibbs_lda_cython",
+                         sources=["gibbs_lda_cython.c"],
+                         include_dirs=[np.get_include()],
+                         libraries=libraries,
+                         extra_compile_args=["-O3"])
+    return config
+
+if __name__ == "__main__":
+    from numpy.distutils.core import setup
+    setup(**configuration().todict())
 
 
-if __name__=="__main__":
-   setup( name = 'gibbs_lda_cython', cmdclass = {'build_ext': build_ext},
-          include_dirs = [np.get_include()], ext_modules = ext_modules )
 
