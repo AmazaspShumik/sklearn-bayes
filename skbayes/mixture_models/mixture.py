fixed underflow

skbayes/mixture_models/mixture.py
@@ -953,7 +953,7 @@ class VBGMMARD(GeneralMixtureModelExponential):
         '''
         d = X.shape[1] 
         # calculate expectation of logdet of precision matrix
-        scale_logdet   = np.linalg.slogdet(scale[k])[1]
+        scale_logdet   = np.linalg.slogdet(scale[k] + np.finfo(np.double).eps)[1]
         e_logdet_prec  = sum([psi(0.5*(dof[k]+1-i)) for i in range(1,d+1)])
         e_logdet_prec += scale_logdet + d*np.log(2)
            