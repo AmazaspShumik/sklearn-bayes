5751b7439e616a19de6e567d9b7cd734fbb40fe8
amazaspshaumyan <sha-amazasp@yandex.ru>
2016-06-28 13:34:37 +0300

    installation of hmm.c

--- setup.py
+++ setup.py
@@ -15,7 +15,13 @@ ext_modules = [ Extension("skbayes.decomposition_models.gibbs_lda_cython",
                           ["skbayes/decomposition_models/gibbs_lda_cython.c"], 
                           #include_dirs = [np.get_include()],
                           extra_compile_args=["-O3"],
-                          **get_info("npymath"))]
+                          **get_info("npymath")),
+                Extension("skbayes.hidden_markov_models.hmm",
+                         ['skbayes/hidden_markov_models/hmm.c'],
+                          extra_compile_args=["-O3"],
+                          **get_info("npymath"))
+              ]
+
 import skbayes
 
  
@@ -42,6 +48,6 @@ if __name__=='__main__':
           'Development Status :: 3 - Alpha',
           'Operating System :: Mac OS X',
           'Programming Language :: Python :: 2.7'],
-       ext_modules = ext_modules
+       ext_modules = cythonize(ext_modules)
    )
 
